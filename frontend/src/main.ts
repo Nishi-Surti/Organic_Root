e491a15366a53a62555738a18592777f573c1613
Nishi-Surti <surtinishita@gmail.com>
2026-05-29 17:30:24 +0530

    minimul angular test

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr } from 'ngx-toastr';
-import { routes } from './app/app.routes';
-import { App } from './app/app';
+import { Component } from '@angular/core';
 
-bootstrapApplication(App,{
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    provideAnimations(),
-    provideToastr()
-  ]
-});
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `<h1>Angular Working 🚀</h1>`
+})
+class TestComponent {}
+
+bootstrapApplication(TestComponent);
