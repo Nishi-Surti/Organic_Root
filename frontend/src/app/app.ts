31110209ebf82d2ae99cc49f3a339d4f710ecfad
Nishi-Surti <surtinishita@gmail.com>
2026-05-29 17:10:26 +0530

    change app.ts file.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,27 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Navbar } from './navbar/navbar';
-import { Footer } from './footer/footer';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Auth } from './services/auth';
-import { CommonModule } from '@angular/common';
-
-
-// @NgModel({
-//   imports: [
-//     FormsModule
-//   ]
-// })
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Navbar,Footer,ReactiveFormsModule,CommonModule],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<h1>Organic Root Working 🚀</h1>`
 })
-export class App {
-  constructor(public authService: Auth) {}
-
-  
-}
+export class App {}
