c189d321deb1d7ce6799802602768ed8465ab501
Nishi-Surti <surtinishita@gmail.com>
2026-05-29 17:35:16 +0530

    fixed angular static output

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Component } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { routes } from './app/app.routes';
+import { App } from './app/app';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `<h1>Angular Working 🚀</h1>`
-})
-class TestComponent {}
-
-bootstrapApplication(TestComponent);
+bootstrapApplication(App,{
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr()
+  ]
+});
